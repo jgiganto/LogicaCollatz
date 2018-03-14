5a0ec16082d620e892a83d4a353a8e809f840924
jgiganto <jgiganto1@hotmail.com>
2018-03-14 11:08:40 +0100

    a

---
+++ LogicaCollatz/IcontratoCollatz.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+
+namespace LogicaCollatz
+{
+    [ServiceContract]
+    public interface IcontratoCollatz
+    {
+        [OperationContract]
+        List<int> GetConjeturaCollatz(int numero);
+        [OperationContract]
+        String GetSaludo();
+        String MetodoSincontrato();
+
+    }
+}
---
+++ LogicaCollatz/ServicioCollatz.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicaCollatz
+{
+    public class ServicioCollatz : IcontratoCollatz
+    {
+        public List<int> GetConjeturaCollatz(int numero)
+        {
+            List<int> datos = new List<int>();
+            datos.Add(numero);
+            while (numero != 1)
+            {
+                if (numero % 2 == 0)
+                {
+                    numero = numero / 2;
+                }
+                else
+                {
+                    numero = numero * 3 + 1;
+                }
+                datos.Add(numero);
+            }
+            return datos;
+        }
+
+        public string GetSaludo()
+        {
+            return "Bienvenido al WCF-post";
+        }
+
+        public string MetodoSincontrato()
+        {
+            return "no me verás jamás";
+        }
+    }
+}
